dbfaa220bef44bc0c298f2c8a673f2e623016ceb
soulseen <zhu_seen@163.com>
2018-04-01 20:55:19 +0800

    create python not com_id

--- job/models/job.py
+++ job/models/job.py
@@ -8,4 +8,11 @@ class Job(Base):
     __tablename__ = "job"
 
     id = Column(Integer, autoincrement=True, primary_key=True)
-    name = Column(String(255), nullable=True)
+    jobname = Column(String(255), nullable=False)
+    money = Column(String(255))
+    url = Column(String(255),nullable=False)
+    origin = Column(String(255),nullable=False)
+    natural = Column(String(255))
+    exp = Column(String(255))
+    education = Column(String(255))
+    time = Column(String(255))
