e2d14d578697cc54cdbaafa7935f1f5c2b413733
soulseen <zhu_seen@163.com>
2018-04-01 15:43:30 +0800

    get url

--- job/spiders/zhilian.py
+++ job/spiders/zhilian.py
@@ -19,24 +19,24 @@ class test(scrapy.Spider):
 
     def start_requests(self):
         yield scrapy.Request(
-            url=self.zhilian.format(**self.data, page="1"),
+            url=self.zhilian.format(**self.data, page=""),
             headers=self.header,
             callback=self.get_page
         )
 
     def get_page(self, response):
-        self.get_url(response)
+        # yield self.get_url(response)
         pattern = re.compile(r'共<em>(\d+)</em>个职位满足条件')
         name = pattern.search(response.body.decode('utf-8'))
         pages = int(int(name.group(1)) / 60) + 2
-        for page in range(2,pages):
+        for page in range(1,pages):
             yield scrapy.Request(
                 url=self.zhilian.format(**self.data,page=page),
                 headers=self.header,
                 callback=self.get_url
             )
 
-    def get_url(self, response):
+    def get_url(self,response):
         job_urls = response.xpath('//a[@par]/@href').extract()
         for job_url in job_urls:
             yield scrapy.Request(
@@ -46,7 +46,5 @@ class test(scrapy.Spider):
             )
 
     def parse(self,response):
-        print(response.url)
-        print('---------')
-        with open('/Users/zhuxiaoyang/zxy/project/Jobsystem/job/export.test','wa') as f:
-            f.write(str(response.url))
+        with open('/Users/zhuxiaoyang/zxy/project/Jobsystem/job/export.txt','a') as f:
+            f.write(str(response.url)+'\n')
