82647d0fcfb6cfb196b45d94ee5d538759bcb5d6
soulseen <zhu_seen@163.com>
2018-04-21 16:29:43 +0800

    incloud python & tongxin

--- job_run.py
+++ job_run.py
@@ -12,7 +12,7 @@ from scrapy.cmdline import execute
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 # execute(['scrapy', 'crawl', 'zhilian_python', '-o', './export/items.json'])
-# execute(['scrapy', 'crawl', 'zhilian_python_beijing'])
+execute(['scrapy', 'crawl', 'zhilian_python_beijing'])
 # execute(['scrapy', 'crawl', 'zhilian_python_shanghai'])
 # execute(['scrapy', 'crawl', 'zhilian_python_shenzhen'])
 # execute(['scrapy', 'crawl', 'zhilian_python_hangzhou'])
@@ -21,8 +21,8 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # execute(['scrapy', 'crawl', 'zhilian_tongxin_beijing'])
 # execute(['scrapy', 'crawl', 'zhilian_tongxin_guangzhou'])
-execute(['scrapy', 'crawl', 'zhilian_tongxin_chengdu'])
 # execute(['scrapy', 'crawl', 'zhilian_tongxin_hangzhou'])
+# execute(['scrapy', 'crawl', 'zhilian_tongxin_chengdu'])
 # execute(['scrapy', 'crawl', 'zhilian_tongxin_shanghai'])
 # execute(['scrapy', 'crawl', 'zhilian_tongxin_shenzhen'])
 
